1b33fd1a4f9f0c5eff560866dfeb007c7fab2972
Kevin Zhuang <kvzhuang@gmail.com>
2013-08-27 11:05:06 +0800

    Add JS code.

--- rich-editor/editor.js
+++ rich-editor/editor.js
@@ -1,41 +1,49 @@
 (function ($) {
     var editor = $('#editor'),
-        container = $('.container'),
-        selectedRange,
 
-        getCurrentRange = function () {
-            var sel = window.getSelection();
-            if (sel.getRangeAt && sel.rangeCount) {
-                return sel.getRangeAt(0);
+    container = $('.container'),
+
+    selectedRange,
+
+    getCurrentRange = function () {
+        var sel = window.getSelection();
+        if (sel.getRangeAt && sel.rangeCount) {
+            return sel.getRangeAt(0);
+        }
+    },
+
+    saveSelection = function () {
+        selectedRange = getCurrentRange();
+    },
+
+    restoreSelection = function () {
+        var selection = window.getSelection();
+        if (selectedRange) {
+            try {
+                selection.removeAllRanges();
+            } catch (ex) {
+                document.body.createTextRange().select();
+                document.selection.empty();
             }
-        },
 
-        saveSelection = function () {
-            selectedRange = getCurrentRange();
-        },
+            selection.addRange(selectedRange);
+        }
+    },
 
-        restoreSelection = function () {
-            var selection = window.getSelection();
-            if (selectedRange) {
-                try {
-                    selection.removeAllRanges();
-                } catch (ex) {
-                    document.body.createTextRange().select();
-                    document.selection.empty();
-                }
-
-                selection.addRange(selectedRange);
-            }
-        },
-
-        exec = function (command, value) {
-
-        };
+    exec = function (command, value) {
+        restoreSelection();
+        document.execCommand(command, false, value);
+    };
 
     editor.on('mouseup keyup mouseout', function () {
         saveSelection();
     });
 
+    container.find('a[data-command]').click(function () {
+        var command = $(this).data('command'),
+        value = $(this).data('value');
+        exec(command, value);
+    });
 
 
 }(window.jQuery));
